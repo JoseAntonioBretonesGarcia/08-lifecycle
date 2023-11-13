09ce89f09aaf3d44a781a7aea56b59854051c727
Jose Antonio Bretones Garcia <joseantoniobregar@gmail.com>
2023-11-13 12:12:34 +0100

    section 20 finished

---
+++ src/app/products/components/price/price.component.ts
@@ -0,0 +1,34 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'product-price',
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.css'],
+})
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input() public price: number = 0;
+  private interval$?: Subscription;
+
+  ngOnInit(): void {
+    console.log('PriceComponent ngOnInit');
+    this.interval$ = interval(1000).subscribe((value) =>
+      console.log(`Tick:${{ value }}`)
+    );
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('PriceComponent ngOnChanges');
+    console.log('PriceComponent changes', changes);
+  }
+  ngOnDestroy(): void {
+    console.log('PriceComponent ngOnDestroy');
+    this.interval$?.unsubscribe();
+  }
+}
